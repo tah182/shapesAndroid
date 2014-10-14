d0ee23af09fba436d77822f20c94fa474dd7e0de
Tah Tatsumoto <tah.tatsumoto@gmail.com>
2014-10-13 23:57:50 -0600

    Initial Commit

---
+++ ShapesGenerator/src/Objects/AbstractShape.java
@@ -0,0 +1,20 @@
+/**
+ * Parent Class AbstractShape. All information regarding shapes will extend this class.
+ * @author 	Tah Tatsumoto
+ * @version	1.0
+ */
+package Objects;
+
+public abstract class AbstractShape {
+	
+	/*
+	 * Calculates new rotation points 
+	 */
+	public void rotate() {
+		
+	}
+	
+	public boolean getRotation(){
+		return false;
+	}
+}
