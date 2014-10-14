8f37a804104a6f913e44e6eb22eacb485ee0b4c4
jamescoot <jamescoot@gmail.com>
2014-10-14 05:33:02 -0600

    - Added properties per Gen My Model UML - Changed rotate method to an abstract method since different child objects might animate differently
    - added constructor with ShapeType enum dependency
    - added accessors and mutators
    
    - mutators and constructor throw unsupported exception for testing
    - no logic was added

--- ShapesGenerator/src/Objects/AbstractShape.java
+++ ShapesGenerator/src/Objects/AbstractShape.java
@@ -6,13 +6,45 @@
 package Objects;
 
 public abstract class AbstractShape {
+	private int rotationSpeed;
+	private int radiusLength;
+	private boolean rotate;
+	private ShapeType shapeType;
+	
+	public AbstractShape(int aRotationSpeed, int aRadiusLength, boolean isRotating, ShapeType aShapeType){
+		// not implemented yet for testing
+		throw new UnsupportedOperationException("not yet implemented");
+	}
+	
+	public int getRotationSpeed() { return rotationSpeed; } 
+	public int getRadiusLength() { return radiusLength; }
+	public boolean isRotating() { return rotate; }
+	public ShapeType getShapeType() { return shapeType; } 
+	
+	public void setRotationSpeed(int aRotationSpeed){
+		// not implemented yet for testing
+		throw new UnsupportedOperationException("not yet implemented");
+	}
+	
+	public void setRadiusLength(int aRadiusLength){
+		// not implemented yet for testing
+		throw new UnsupportedOperationException("not yet implemented");
+	}
+	
+	public void setRotation(boolean isRotating){
+		// not implemented yet for testing
+		throw new UnsupportedOperationException("not yet implemented");
+	}
+	
+	public void setShapeType(ShapeType aShapeType){
+		// not implemented yet for testing
+		throw new UnsupportedOperationException("not yet implemented");
+	}
 	
 	/*
 	 * Calculates new rotation points 
 	 */
-	public void rotate() {
-		
-	}
+	public abstract void rotate();	
 	
 	public boolean getRotation(){
 		return false;
