dec6990cb63ebd66e97a25e8ea7faebc769e6798
jamescoot <jamescoot@gmail.com>
2014-10-25 10:08:52 -0600

    Added Unit Test

---
+++ ShapesGenerator/UnitTests/ShapeTest.java
@@ -0,0 +1,118 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class ShapeTest {
+
+	@Test
+	public void testRotate() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testShapeIntIntBooleanShapeType() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testShapeIntIntBooleanShapeTypePoint() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetCenterPoint() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetEndPoints() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetCenterPoint() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAddEndPoint() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRemoveEndPoint() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testEqualsObject() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCompareTo() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAbstractShape() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetRotationSpeed() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetRadiusLength() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testIsRotating() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetShapeType() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetPath() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetRotationSpeed() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetRadiusLength() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetRotation() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetShapeType() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetPath() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetRotation() {
+		fail("Not yet implemented");
+	}
+
+}
