440935a131b7f9f16b90101113f56dff09bd8c8d
Tah Tatsumoto <tah.tatsumoto@gmail.com>
2014-10-25 00:18:39 -0600

    Updated Shapes, AbstractShape with new constructors

--- ShapesGenerator/src/Objects/AbstractShape.java
+++ ShapesGenerator/src/Objects/AbstractShape.java
@@ -24,48 +24,54 @@ public abstract class AbstractShape {
 		this.shapeType = _shapeType;
 	}
 	
+	public boolean getRotation(){
+		return false;
+	}
 	public int getRotationSpeed() {
-		return rotationSpeed; }
+		return rotationSpeed; 
+	}
 	public int getRadiusLength() {
-		return radiusLength; }
+		return radiusLength; 
+	}
 	public boolean isRotatingClockwise() { 
-		return clockwiseRotation; }
+		return clockwiseRotation; 
+	}
 	public ShapeType getShapeType() {
-		return shapeType; }
+		return shapeType; 
+	}
 	public LinearVector getPath() {
-		return path; }
-	
-	public void setRotationSpeed(int _rotationSpeed){
-		// not implemented yet for testing
-		throw new UnsupportedOperationException("not yet implemented");
+		return path; 
 	}
 	
-	public void setRadiusLength(int _radiusLength){
-		// not implemented yet for testing
-		throw new UnsupportedOperationException("not yet implemented");
-	}
+	// Deprechated, these properties will be immutable
+//	public void setRotationSpeed(int _rotationSpeed){
+//		// not implemented yet for testing
+//		throw new UnsupportedOperationException("not yet implemented");
+//	}
+//	
+//	public void setRadiusLength(int _radiusLength){
+//		// not implemented yet for testing
+//		throw new UnsupportedOperationException("not yet implemented");
+//	}
+//	
+//	public void setRotation(boolean isRotating){
+//		// not implemented yet for testing
+//		throw new UnsupportedOperationException("not yet implemented");
+//	}
+//	
+//	public void setShapeType(ShapeType _shapeType){
+//		// not implemented yet for testing
+//		throw new UnsupportedOperationException("not yet implemented");
+//	}
+//	
+//	public void setPath(LinearVector _path){
+//		// not implemented yet for testing
+//		throw new UnsupportedOperationException("not yet implemented");		
+//	}
 	
-	public void setRotation(boolean isRotating){
-		// not implemented yet for testing
-		throw new UnsupportedOperationException("not yet implemented");
-	}
-	
-	public void setShapeType(ShapeType _shapeType){
-		// not implemented yet for testing
-		throw new UnsupportedOperationException("not yet implemented");
-	}
-	
-	public void setPath(LinearVector _path){
-		// not implemented yet for testing
-		throw new UnsupportedOperationException("not yet implemented");		
-	}
-	
-	/*
+	/**
 	 * Calculates new rotation points 
 	 */
 	public abstract void rotate();	
 	
-	public boolean getRotation(){
-		return false;
-	}
 }
--- ShapesGenerator/src/Objects/Shape.java
+++ ShapesGenerator/src/Objects/Shape.java
@@ -8,7 +8,15 @@
 
 package Objects;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.LinkedList;
+import java.util.List;
+import java.util.Random;
+
+import com.example.shapesgenerator.BuildConfig;
+
 import android.graphics.Point;
 
 public class Shape extends AbstractShape implements Comparable<Shape> {
@@ -19,8 +27,10 @@ public class Shape extends AbstractShape implements Comparable<Shape> {
 				 int _radiusLength, 
 				 boolean isRotating, 
 				 ShapeType _shapeType) {
-		
 		super(_rotationSpeed, _radiusLength, isRotating, _shapeType);
+		
+		//TODO write generation of centerPoint randomly.
+		// must be within the bounds of the screen-radius;
 	}
 	
 	public Shape(int _rotationSpeed, 
@@ -28,12 +38,41 @@ public class Shape extends AbstractShape implements Comparable<Shape> {
 				 boolean isRotating, 
 				 ShapeType _shapeType, 
 				 Point _centerPoint) {
-		
 		super(_rotationSpeed, _radiusLength, isRotating, _shapeType);
+		
+		this.centerPoint = _centerPoint;
+	}
+	
+	public Shape() {
+		this(getRandomInt(100), 
+			 getRandomInt(100), 
+			 getRandomInt(10) % 2 == 0, 
+			 getRandomShapeType());
+	}
+	
+	/**
+	 * Generates a random number between 0 and the upperBound
+	 * @param upperBound the upperBound of the random int
+	 * @return the random number generated
+	 */
+	private static int getRandomInt(int upperBound) {
+		Random r = new Random();
+		return r.nextInt() * upperBound;
+	}
+	
+	private static ShapeType getRandomShapeType() {
+		// Generate list of Enums and call Shuffle property of List
+		List<ShapeType> shapes = new ArrayList<ShapeType>(Arrays.asList(ShapeType.values()));
+		Collections.shuffle(shapes);
+		
+		if (shapes.size() > 0) 
+			return shapes.get(0);
+		
+		throw new ArrayIndexOutOfBoundsException ("Random Generation of ShapeTypes could not generate.");
 	}
 
-	public Point getCenterPoint(){ return centerPoint; } 
-	public Point[] getEndPoints(){ return (Point[])endPoints.toArray(); }
+	public Point getCenterPoint()	{ return centerPoint; } 
+	public Point[] getEndPoints()	{ return (Point[])endPoints.toArray(); }
 	
 	
 	public void setCenterPoint(Point c) { 
